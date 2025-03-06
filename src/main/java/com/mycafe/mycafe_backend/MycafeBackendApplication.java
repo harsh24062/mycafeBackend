3b87bccd15c0fe6769a434dd112c30bfd732ab8f
harsh24062 <129416678+harsh24062@users.noreply.github.com>
2025-03-06 15:28:56 +0530

    Initial Commit

---
+++ src/main/java/com/mycafe/mycafe_backend/MycafeBackendApplication.java
@@ -0,0 +1,13 @@
+package com.mycafe.mycafe_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MycafeBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MycafeBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mycafe/mycafe_backend/MycafeBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mycafe.mycafe_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MycafeBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
